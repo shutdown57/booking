6b13478612d798fe4e74b84b72c47831185fe5cc
Majid Mohamadi Parvar <m.shutdown57@gmail.com>
2024-07-29 02:28:47 +0330

    chore(web): add breeze routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,51 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TennisCourtController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    function () {
+        return Inertia::render(
+            'Welcome',
+            [
+                'canLogin' => Route::has('login'),
+                'canRegister' => Route::has('register'),
+                'laravelVersion' => Application::VERSION,
+                'phpVersion' => PHP_VERSION,
+            ]
+        );
+    }
+);
+
+Route::get(
+    '/dashboard',
+    function () {
+        return Inertia::render('Dashboard');
+    }
+)->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    }
+);
+
+Route::group(
+    [
+        'prefix' => 'tennis-court'
+    ],
+    function () {
+        Route::get('/', [TennisCourtController::class, 'index'])
+            ->name('tennis-court.index');
+        Route::get('/create', [TennisCourtController::class, 'create'])
+            ->name('tennis-court.index');
+    }
+);
+
+require __DIR__.'/auth.php';
