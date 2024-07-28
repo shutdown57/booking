95f2e68efe36fb146444976c86e1bbc4ea7d4662
Majid Mohamadi Parvar <m.shutdown57@gmail.com>
2024-07-29 02:25:07 +0330

    chore(inertia): add generated basic layouts

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div
+        class="flex flex-col items-center pt-6 min-h-screen bg-gray-100 sm:justify-center sm:pt-0"
+    >
+        <div>
+            <Link href="/"> Home Page </Link>
+        </div>
+
+        <div
+            class="overflow-hidden py-4 px-6 mt-6 w-full bg-white shadow-md sm:max-w-md sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
